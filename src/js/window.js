562d7229cc79afda49fa78d2929bff70e7ef1a55
fukayatsu <fukayatsu@gmail.com>
2014-02-05 15:20:18 +0900

    first commit

---
+++ src/js/window.js
@@ -0,0 +1,8 @@
+var resize_content = function() {
+  var content = document.getElementById('content');
+  content.style.width  = (window.document.documentElement.clientWidth  - 16) + 'px'
+  content.style.height = (window.document.documentElement.clientHeight - 16) + 'px'
+}
+
+window.onload   = resize_content;
+window.onresize = resize_content;
