7cbaa906f77aad4a4e11c5e66adab23155565d9a
fukayatsu <fukayatsu@gmail.com>
2014-02-07 00:32:28 +0900

    Add keyboard shortcut, open external link

--- src/js/window.js
+++ src/js/window.js
@@ -4,5 +4,31 @@ var resize_content = function() {
   content.style.height = (window.document.documentElement.clientHeight - 16) + 'px'
 }
 
-window.onload   = resize_content;
+window.onload   = function() {
+  resize_content;
+  var webview = document.getElementById("webview");
+  document.addEventListener("keydown" , function(event) {
+    var key = event.keyCode || event.charCode || 0;
+    if (!event.ctrlKey && !event.metaKey) { return; }
+
+    switch(key) {
+      //  R: reload
+      case 82: webview.reload(); break;
+      //  F: zapping
+      case 70: webview.executeScript({ code: "location.pathname = '/zapping';" }); break;
+      //  D: did read
+      case 68:
+        webview.executeScript({
+          code: "document.getElementsByClassName('post-star')[0].click();"
+        });
+        break;
+    }
+  });
+
+  webview.addEventListener('newwindow', function(e) {
+    e.preventDefault();
+    window.open(e.targetUrl)
+  });
+
+}
 window.onresize = resize_content;
